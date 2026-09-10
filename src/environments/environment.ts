8a73ecf3cfa8a91701e1ccade73b4830a3ef5eb8
Bruno Casé <bruno.case45@gmail.com>
2026-09-10 20:14:59 -0300

    hotfix - ip ecs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://34.205.78.54:8080/lunaLink'
+  apiUrl: 'https://2b3l0my3e3.execute-api.us-east-1.amazonaws.com/prod/lunaLink'
 };
